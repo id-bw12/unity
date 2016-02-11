5921f555a5aa6783afded0bc18f341ec0fcdbd85
id-bw12 <eddiemeza7@gmail.com>
2016-02-11 11:31:09 -0800

    push "Script changes"
    makes the sphere in run time

--- Assets/cubeBehavior.cs
+++ Assets/cubeBehavior.cs
@@ -14,6 +14,17 @@ public class cubeBehavior : MonoBehaviour {
 
         cube.transform.position = new Vector3(3,0,0);
 
+        var cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+        cylinder.AddComponent<Rigidbody>();
+
+        cylinder.transform.position = new Vector3(-3, 0, 0);
+
+        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        sphere.AddComponent<Rigidbody>();
+
+        sphere.transform.position = new Vector3(5, 0, 0);
     }
 	
 	// Update is called once per frame

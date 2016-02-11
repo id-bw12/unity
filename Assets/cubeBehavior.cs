53ee2e72dbd86a61aec7db9c69a93cac543b243a
id-bw12 <eddiemeza7@gmail.com>
2016-02-11 11:57:48 -0800

    push "camera position"
    camera position changed

--- Assets/cubeBehavior.cs
+++ Assets/cubeBehavior.cs
@@ -3,32 +3,37 @@ using System.Collections;
 
 public class cubeBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-        print("Hi there.");
 
-        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+
+
+    // Use this for initialization
+    void Start () {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        print("Hi there.");
 
         cube.AddComponent<Rigidbody>();
 
         cube.transform.position = new Vector3(3,0,0);
 
-        var cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-
         cylinder.AddComponent<Rigidbody>();
 
-        cylinder.transform.position = new Vector3(-3, 0, 0);
-
-        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        cylinder.transform.position = new Vector3(1, 0, 5);
 
         sphere.AddComponent<Rigidbody>();
 
         sphere.transform.position = new Vector3(5, 0, 0);
+
+        
+
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
 }

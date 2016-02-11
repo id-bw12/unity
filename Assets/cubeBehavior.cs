93ef7a9138a9bfa06ed8fd259527f492e6842367
id-bw12 <eddiemeza7@gmail.com>
2016-02-11 11:02:08 -0800

    add "push script"
    Adds a new script change

---
+++ Assets/cubeBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+        print("Hi there.");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
